c88868675806aeff82a118a5ef30940b1f6fba6f
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-06-04 17:39:05 +0300

    Adding routering for the links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <>
+        <Header />
+        <main>
+            <Routes> 
+              <Route path="/" element={<Main />} />
+              {/* <Route path="/about" element={<AboutPage />} />
+              <Route path="/menu" element={<MenuPage />} />
+              <Route path="/reservations" element={<ReservationsPage />} />
+              <Route path="/order-online" element={<OrderOnlinePage />} />
+              <Route path="/login" element={<LoginPage />} /> */}
+            </Routes>
+          </main>
+        <Main />
+        <Footer />
+      </>
+    </BrowserRouter>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const navLinks = [
@@ -14,8 +15,18 @@ const Header = () => {
     return (
         <header className='container'>
             {/* Assuming you have a logo image named "logo.png" in your public folder */}
-            <img src="/logo.png" alt="Little Lemon Logo" />
-            <Nav navLinks={navLinks} />
+            <Link to="/">
+                <img src="/logo.png" alt="Little Lemon Logo" />
+            </Link>
+            <nav>
+                <ul>
+                    {navLinks.map((link, index) => (
+                    <li key={index}>
+                        <Link to={link.url}>{link.text}</Link>
+                    </li>
+                    ))}
+                </ul>
+            </nav>
         </header>
     )
 }
